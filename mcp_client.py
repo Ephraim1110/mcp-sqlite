4003ef115851952e0b342061333c1f839c0b1f10
Ephraim Goussanou <ephraimgoussanou0@gmail.com>
2025-07-05 02:40:24 +0200

    add agent functionality to interact with MCP and retrieve user data

--- mcp_client.py
+++ mcp_client.py
@@ -3,7 +3,7 @@ from gradio_client import Client
 
 client = Client("http://127.0.0.1:7860/")
 
-# Tester ton seul tool : afficher_users
+
 result = client.predict(api_name="/afficher_users")
 
 print("Résultat MCP Server :", result)
